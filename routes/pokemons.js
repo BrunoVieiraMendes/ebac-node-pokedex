9868b557e495be78682601798c8590dc21f35db4
Bruno Mendes <bruno.vieira.mendes10@gmail.com>
2025-04-22 20:38:13 -0300

    commit aula: 9 colocamos a data que os pokemons foram capturados

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -5,6 +5,14 @@ const router = express.Router();
 
 router.get('/', (_, res) => {
     Pokemon.find().then(pokemon => {
+       pokemon.forEach(pokemon => {
+        const dataIso = pokemon._id.getTimestamp().toISOString();
+        const dataInfo = dataIso.split('T')[0];
+        const dataEmPedacos = dataInfo.split('-');
+
+        pokemon.capturadoEm = `${dataEmPedacos[2]}/${dataEmPedacos[1]}/${dataEmPedacos[0]}`;
+       });
+       
         res.render('paginas/pokemons/index', {
             pokemons: pokemon, 
         });
