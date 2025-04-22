c253f18784be034f848d840d9e5914a78a201e6d
Bruno Mendes <bruno.vieira.mendes10@gmail.com>
2025-04-22 00:09:59 -0300

    nesse commit foi adicionado informações no arquivo batalha.js e na busca-pokemon.js, tambem foi adicionado a biblioteca axios e novas views

--- routes/batalha.js
+++ routes/batalha.js
@@ -1,9 +1,17 @@
 const express = require('express');
 
+const buscaInfoPokemon = require('../services/busca-pokemon');
+
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-    console.log(Math.random());
+    const pokemonIdRandomico = (Math.round(Math.random() * 904 + 1));
+    
+    buscaInfoPokemon(pokemonIdRandomico).then(pokemon => {
+        res.render('paginas/batalha/index', {
+            pokemon,
+        });
+    })
     res.send({});
 });
 
---
+++ services/busca-pokemon.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+
+const buscaInfoPokemon = (pokeId) => {
+    return new Promise((resolve,reject) => {
+
+        const url = "https://pokeapi.co/api/v2/pokemon/" + pokeId;
+
+        axios.get(url).then(resultado => {
+            const data = resultado.data;
+    
+            const id = data.id;
+            const nome = data.name;
+            const altura = data.height;
+            const peso = data.weight;
+            const imagem = data.sprites.front_default;
+            const ataques = data.abilities.map(a => a.ability.name).join(', ');
+    
+            const estatisticas = {};
+    
+            data.stats.forEach((estatistica) => {
+                estatisticas[estatistica.stat.name] = estatistica.base_stat;
+    
+            });
+            resolve({
+                id: id,
+                nome: nome,
+                altura: altura,
+                peso: peso,
+                imagem: imagem,
+                ataques: ataques,
+                estatisticas: estatisticas,
+            })
+
+        }).catch((e) => reject(e));
+
+    });
+  
+};
+
+module.exports = buscaInfoPokemon;
