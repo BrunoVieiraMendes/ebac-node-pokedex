cc2c17ceb00a38e94b4360f0e8cf4d68932d948e
Bruno Mendes <bruno.vieira.mendes10@gmail.com>
2025-08-18 23:04:47 -0300

    try catch no arquivo pokemons.js

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -18,18 +18,20 @@ router.get('/', async (_req, res) => {
         });
     });
 
-router.get('/:id', (req, res) => {
-    Pokemon.findOne({ _id: req.params.id }).then(pokemon => {
+router.get('/:id', async (req, res) => {
+   try{
+    const pokemon = await Pokemon.findOne({ _id: req.params.id });
+    
         res.render("paginas/pokemons/show", {
           pokemon,
             message: req.query.message,
         });
-    }).catch(e => {
-        res.status(404).render('paginas/erro', {
+    } catch (e) {
+       res.status(404).render('paginas/erro', {
             mensagem: "Pokemon não encontrado !!!",
             erro: {},
-        });
-    });
+        }); 
+    }
 });
 
 module.exports = router;
