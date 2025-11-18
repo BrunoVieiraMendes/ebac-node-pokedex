a142f27cf861f1ea13aa0c757ed568f3ec0be813
Bruno Vieira Mendes <bruno.vieira.mendes10@gmail.com>
2025-11-18 01:30:20 -0300

    adicionando dados com seeds.

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -14,7 +14,7 @@ router.get('/', async (_req, res) => {
        });
        
         res.render('paginas/pokemons/index', {
-            pokemons: pokemon, 
+            pokemons: pokemons, 
         });
     });
 
